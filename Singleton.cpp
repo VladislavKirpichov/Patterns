2ee5f40c98b48d05fc51cf38b2983e06c4e86d85
Vladislav <sdvlad75@gmail.com>
2022-07-07 20:38:07 +0300

    Edited Singleton

--- Singleton.cpp
+++ Singleton.cpp
@@ -3,11 +3,14 @@
 //
 
 #include <iostream>
+#include <mutex>
+#include <thread>
 
 class Singleton {
-public:
+protected:
     Singleton() = default;
 
+public:
     static Singleton get_instance() {
         static Singleton singleton{};
         singleton.check++;
@@ -17,10 +20,87 @@ public:
     int check = 0;
 };
 
-int main() {
+class SingletonWithCounter {
+public:
+    static SingletonWithCounter* get_instance() {
+        if (count == 0) {
+            singleton = new SingletonWithCounter();
+            count++;
+        }
+
+        return singleton;
+    }
+
+    int test_counter = 0;
+
+private:
+    SingletonWithCounter() = default;
+
+    static inline unsigned count = 0;
+    static inline SingletonWithCounter* singleton = nullptr;
+};
+
+
+class SingletonWithMutex {
+public:
+    static SingletonWithMutex* get_instance() {
+        mutex.lock();
+
+        if (singleton == nullptr)
+            singleton = new SingletonWithMutex();
+
+        mutex.unlock();
+        return singleton;
+    }
+
+    int test_counter = 0;
+
+private:
+    SingletonWithMutex() = default;
+
+    static inline std::mutex mutex{};
+    static inline SingletonWithMutex* singleton = nullptr;
+};
+
+void test_simple_singleton() {
     Singleton test_1 = Singleton::get_instance();
     Singleton test_2 = Singleton::get_instance();
 
     // Если бы создались 2 разных объекта, то вывелось 1 1
     std::cout << test_1.check << ' ' << test_2.check << '\n';   // 1 2
+}
+
+void test_counter_singleton() {
+    SingletonWithCounter* test = SingletonWithCounter::get_instance();
+    SingletonWithCounter* another_test = SingletonWithCounter::get_instance();
+
+    test->test_counter++;
+    another_test->test_counter++;
+    another_test->test_counter++;
+
+    // Если бы создались 2 разных объекта, то вывелось 1 2
+    std::cout << test->test_counter << ' ' << another_test->test_counter << '\n';   // 3 3
+}
+
+void test_mutex() {
+    SingletonWithMutex* test = SingletonWithMutex::get_instance();
+    test->test_counter += 1;
+}
+
+void test_mutex_singleton() {
+    SingletonWithMutex* test_mutex_1 = SingletonWithMutex::get_instance();
+    SingletonWithMutex* test_mutex_2 = SingletonWithMutex::get_instance();
+
+    std::thread thread(test_mutex);
+    thread.join();
+
+    std::cout << test_mutex_1->test_counter << ' ' << test_mutex_2->test_counter << '\n';   // 1 1
+}
+
+int main() {
+    test_simple_singleton();
+    test_counter_singleton();
+    test_mutex_singleton();
+
+    return 0;
 }
